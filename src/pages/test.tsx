9ebaa0dad49c02beae9291c2a544afb66d621361
Arvedson <tomasarvedson@gmail.com>
2024-01-16 12:39:17 -0600

    test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,16 +1,7 @@
 // FuncionQueUtilizaComponente.js
 import React from 'react';
 
-import Frame15 from './Frame15';
-import Componente1 from './Componente1';
-import Frame14 from './Frame14';
-import Frame13 from './Frame13';
-import Frame39 from './Frame39';
-import Frame50 from './Frame50';
-import Header1 from './Header1';
-import Dropdown from './Dropdown';
-import Homescreen from './Homescreen';
-import Headeer from './Headeer';
+
 import Seccion1 from "./Seccion1"
 import Seccion2 from './Seccion2';
 
