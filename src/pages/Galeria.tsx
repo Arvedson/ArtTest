e01621b77dbd8f9a6d39f0c4a4bc6225a36cd6d2
Arvedson <tomasarvedson@gmail.com>
2024-01-18 12:59:30 -0600

    seccion3test

--- src/pages/Galeria.tsx
+++ src/pages/Galeria.tsx
@@ -11,21 +11,27 @@ interface CuadroProps {
   tamañoCuadro: string;
 }
 
-const Galeria: React.FC<{ cuadro: CuadroProps }> = ({ cuadro }) => {
-  
+const Galeria: React.FC<{ cuadro?: CuadroProps }> = ({ cuadro }) => {
+  // Verificar si cuadro está definido
+  if (!cuadro) {
+    return null; // o puedes manejar este caso de alguna otra manera según tu lógica
+  }
+
+  // Desestructuración del objeto cuadro
   const { nombreCuadro, imagenCuadro, coloresCuadro, tecnicaCuadro, tamañoCuadro } = cuadro;
 
   return (
     <div className={styles.componente} /* componente */>
-   
+      {/* Título del cuadro */}
       <h3 className={styles.nombrecuadro}>{nombreCuadro}</h3>
 
-     
+      {/* Imagen principal del cuadro */}
       <Image className={styles.imagenCuadro} src={imagenCuadro} alt={nombreCuadro} width={300} height={300} />
 
+      {/* Detalles del cuadro */}
       <div className={styles.propiedades}>
         <ul className={styles.listaDetalles}>
-   
+          {/* Detalle de colores */}
           <li className={styles.detalleItem}>
             <span className={styles.textoDetalle}>Colores:</span>
             <div className={styles.coloresCuadro}>
@@ -35,16 +41,16 @@ const Galeria: React.FC<{ cuadro: CuadroProps }> = ({ cuadro }) => {
             </div>
           </li>
 
-     
+          {/* Detalle de técnica */}
           <li className={styles.textoDetalle2}>{tecnicaCuadro}</li>
-          <li className={styles.textoDetalle2}>{tamañoCuadro}</li>
 
+          {/* Detalle de tamaño */}
+          <li className={styles.textoDetalle2}>{tamañoCuadro}</li>
         </ul>
       </div>
-
-      
     </div>
   );
 };
+
 
 export default Galeria;
